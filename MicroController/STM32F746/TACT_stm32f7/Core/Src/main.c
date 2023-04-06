fd9c28d2d67dcd1a2e429f96be6c3642350f6b0f
Jae Hyeong Park <csp00275@skku.edu>
2023-04-06 14:39:35 +0900

    multimux

--- MicroController/STM32F746/TACT_stm32f7/Core/Src/main.c
+++ MicroController/STM32F746/TACT_stm32f7/Core/Src/main.c
@@ -19,7 +19,6 @@
 ///* Includes ------------------------------------------------------------------*/
 //#include "main.h"
 //#include "i2c.h"
-//#include "tim.h"
 //#include "usart.h"
 //#include "gpio.h"
 //
@@ -28,6 +27,11 @@
 //
 //#include <stdio.h>
 //#include "vl53l0x_api.h"
+//
+//
+//
+//#include "230320_06_quantized.cc"
+//
 //
 ///* USER CODE END Includes */
 //
@@ -39,9 +43,10 @@
 ///* Private define ------------------------------------------------------------*/
 ///* USER CODE BEGIN PD */
 //
-//#define TCA9548A_ADDR 	0x70 << 1 // Default I2C address of TCA9548A
-//#define VL53L0X_ADDR	0x52 // Default I2C address of VL53L0X
-//#define NUM_SENSOR		4
+//
+//#define VL53L0X_ADDR	0x29 << 1 // Default I2C address of VL53L0X
+//#define NUM_SENSOR		24
+//
 //
 ///* USER CODE END PD */
 //
@@ -49,41 +54,12 @@
 ///* USER CODE BEGIN PM */
 //
 //
-//  void init_VL53L0X(VL53L0X_DEV* Dev, VL53L0X_Dev_t* vl53l0x_s, uint8_t* tca_ch, uint32_t refSpadCount, uint8_t isApertureSpads, uint8_t VhvSettings, uint8_t PhaseCal)
-//  {
-//	    for (int i = 0; i < NUM_SENSOR; i++) {
-//
-//	    HAL_I2C_Master_Transmit(&hi2c1, TCA9548A_ADDR, &tca_ch[i], 1, 1000);
-//
-//	  	Dev[i] = &vl53l0x_s[i];
-//	  	Dev[i]->I2cHandle = &hi2c1;
-//	  	Dev[i]->I2cDevAddr = VL53L0X_ADDR;
-//
-//	    VL53L0X_WaitDeviceBooted( Dev[i] );
-//	    VL53L0X_DataInit( Dev[i] );
-//	    VL53L0X_StaticInit( Dev[i] );
-//	    VL53L0X_PerformRefCalibration( Dev[i], &VhvSettings, &PhaseCal);
-//	    VL53L0X_PerformRefSpadManagement( Dev[i], &refSpadCount, &isApertureSpads);
-//	    VL53L0X_SetDeviceMode( Dev[i], VL53L0X_DEVICEMODE_SINGLE_RANGING);
-//	    VL53L0X_SetLimitCheckEnable( Dev[i], VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, 1);
-//	    VL53L0X_SetLimitCheckEnable( Dev[i], VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, 1);
-//	    VL53L0X_SetLimitCheckValue( Dev[i], VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, (FixPoint1616_t)(0.1*65536));
-//	    VL53L0X_SetLimitCheckValue( Dev[i], VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, (FixPoint1616_t)(60*65536));
-//	    VL53L0X_SetMeasurementTimingBudgetMicroSeconds( Dev[i], 33000);
-//	    VL53L0X_SetVcselPulsePeriod( Dev[i], VL53L0X_VCSEL_PERIOD_PRE_RANGE, 18);
-//	    VL53L0X_SetVcselPulsePeriod( Dev[i], VL53L0X_VCSEL_PERIOD_FINAL_RANGE, 14);
-//	    }
-//
-//  }
-//
 ///* USER CODE END PM */
 //
 ///* Private variables ---------------------------------------------------------*/
 //
 ///* USER CODE BEGIN PV */
 //
-///* Flag for TIM7 */
-//volatile uint8_t TIM7_flag = 0;
 //
 ///* UART1 rx data */
 //uint8_t rx1_data;
@@ -95,24 +71,19 @@
 //VL53L0X_RangingMeasurementData_t RangingData;
 //
 //
-//VL53L0X_Dev_t vl53l0x_s[ NUM_SENSOR]; // 센서 배열
-//VL53L0X_DEV Dev[NUM_SENSOR];
-//
-////VL53L0X_DEV Dev[NUM_SENSOR] = {&vl53l0x_s[0], &vl53l0x_s[1], &vl53l0x_s[2], &vl53l0x_s[3], &vl53l0x_s[4], &vl53l0x_s[6]}; // 센서에 대한 포인터 배열
-////VL53L0X_DEV Dev[8] = {&vl53l0x_s[0], &vl53l0x_s[1], &vl53l0x_s[2], &vl53l0x_s[3], &vl53l0x_s[4], &vl53l0x_s[5], &vl53l0x_s[6], &vl53l0x_s[7]}; // 센서에 대한 포인터 배열
-//uint8_t tca_ch[NUM_SENSOR];
 //
 //
 //
-////uint8_t tca_ch[NUM_SENSOR] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80}; // control register of TCA9548A
-//// uint8_t tca_ch[8] = {0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000};
+////uint8_t tca_ch[8] = {0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000};
+////uint8_t tca_ch_reset = 0b00000000;
+//
+//
 //
 //
 ///* USER CODE END PV */
 //
 ///* Private function prototypes -----------------------------------------------*/
 //void SystemClock_Config(void);
-//static void MX_NVIC_Init(void);
 ///* USER CODE BEGIN PFP */
 //
 ///* Setting for "printf" */
@@ -121,11 +92,6 @@
 //
 ///* Private user code ---------------------------------------------------------*/
 ///* USER CODE BEGIN 0 */
-//
-//
-////------ GPT answer BEGIN ----//
-//
-////------ GPT answer END ----//
 //
 ///* USER CODE END 0 */
 //
@@ -136,13 +102,8 @@
 //int main(void)
 //{
 //  /* USER CODE BEGIN 1 */
-//	// VL53L0X initialization stuff
-//	//
-//    uint32_t refSpadCount = 0;
-//    uint8_t isApertureSpads = 0;
-//    uint8_t VhvSettings = 0;
-//    uint8_t PhaseCal = 0;
-//    uint16_t distance[2];
+//
+//
 //
 //
 //
@@ -154,6 +115,26 @@
 //  HAL_Init();
 //
 //  /* USER CODE BEGIN Init */
+//
+//
+//	// VL53L0X initialization stuff
+//	//
+//	uint32_t refSpadCount = 0;
+//	uint8_t isApertureSpads = 0;
+//	uint8_t VhvSettings = 0;
+//	uint8_t PhaseCal = 0;
+//
+//	VL53L0X_Dev_t vl53l0x_s[NUM_SENSOR]; // ?��?�� 배열
+//	VL53L0X_DEV Dev;
+//	uint16_t distance[NUM_SENSOR] = {0,};
+//
+//	uint8_t tca_ch[8] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80}; // control register of TCA9548A
+//	uint8_t tca_ch_reset = 0x00;
+//
+//    float output_data[3];
+//
+//
+//
 //
 //
 //
@@ -170,37 +151,66 @@
 //  /* Initialize all configured peripherals */
 //  MX_GPIO_Init();
 //  MX_I2C1_Init();
-//  MX_TIM7_Init();
 //  MX_USART1_UART_Init();
-//
-//  /* Initialize interrupts */
-//  MX_NVIC_Init();
 //  /* USER CODE BEGIN 2 */
 //
 //
-//  HAL_GPIO_WritePin(GPIOI, GPIO_PIN_1, RESET);
 //
-//  /* Timer Interrupt */
-//  HAL_TIM_Base_Start_IT(&htim7);
+//
+//
+//
 //
 //  /* UART interrupt initialization */
-//  MessageLen = sprintf((char*)Message, "msalamon.pl VL53L0X test\n\r");
+//  MessageLen = sprintf((char*)Message, "JH VL53L0X test\n\r");
 //
 //  HAL_UART_Transmit(&huart1, Message, MessageLen, 100);
 //
 //
-//  // Initialize the TCA9548A multiplexer
-//  for (int i = 0; i < NUM_SENSOR; i++) {
-//      Dev[i] = &vl53l0x_s[i];
-//  }
 //
-//  for (int i = 0; i < NUM_SENSOR; i++) {
-//      tca_ch[i] = 1 << i;
-//  }
+//	  for(int addr = 0x70; addr < 0x73; addr ++){
+//		HAL_I2C_Master_Transmit(&hi2c1, addr << 1, &tca_ch_reset, 1, 1000);
+//	  }
 //
-//  // Initialize the TCA9548A multiplexer and VL53L0X ToF sensors
 //
-//  init_VL53L0X(Dev, vl53l0x_s, tca_ch, refSpadCount, isApertureSpads, VhvSettings, PhaseCal);
+//		for (int i = 0; i < NUM_SENSOR; i++) {
+//
+//			uint8_t q = i / 8;
+//			uint8_t r = i % 8;
+//
+//			if(q == 0){
+//				HAL_I2C_Master_Transmit(&hi2c1, 0x70 << 1, &tca_ch[r], 1, 1000);
+//				HAL_I2C_Master_Transmit(&hi2c1, 0x71 << 1, &tca_ch_reset, 1, 1000);
+//				HAL_I2C_Master_Transmit(&hi2c1, 0x72 << 1, &tca_ch_reset, 1, 1000);
+//
+//			}else if(q == 1){
+//				HAL_I2C_Master_Transmit(&hi2c1, 0x70 << 1, &tca_ch_reset, 1, 1000);
+//				HAL_I2C_Master_Transmit(&hi2c1, 0x71 << 1, &tca_ch[r], 1, 1000);
+//				HAL_I2C_Master_Transmit(&hi2c1, 0x72 << 1, &tca_ch_reset, 1, 1000);
+//
+//			}else if(q == 2){
+//				HAL_I2C_Master_Transmit(&hi2c1, 0x70 << 1, &tca_ch_reset, 1, 1000);
+//				HAL_I2C_Master_Transmit(&hi2c1, 0x71 << 1, &tca_ch_reset, 1, 1000);
+//				HAL_I2C_Master_Transmit(&hi2c1, 0x72 << 1, &tca_ch[r], 1, 1000);
+//			}
+//
+//			Dev = &vl53l0x_s[i];
+//			Dev->I2cHandle = &hi2c1;
+//			Dev->I2cDevAddr = VL53L0X_ADDR;
+//
+//			VL53L0X_WaitDeviceBooted( Dev );
+//			VL53L0X_DataInit( Dev );
+//			VL53L0X_StaticInit( Dev );
+//			VL53L0X_SetDeviceMode(Dev, VL53L0X_DEVICEMODE_CONTINUOUS_RANGING);
+//			VL53L0X_PerformRefCalibration( Dev, &VhvSettings, &PhaseCal);
+//			VL53L0X_PerformRefSpadManagement( Dev, &refSpadCount, &isApertureSpads);
+//			VL53L0X_SetLimitCheckEnable( Dev, VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, 1);
+//			VL53L0X_SetLimitCheckEnable( Dev, VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, 1);
+//			VL53L0X_SetLimitCheckValue( Dev, VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, (FixPoint1616_t)(0.1*65536));
+//			VL53L0X_SetLimitCheckValue( Dev, VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, (FixPoint1616_t)(60*65536));
+//			VL53L0X_SetMeasurementTimingBudgetMicroSeconds( Dev, 33000);
+//			VL53L0X_SetVcselPulsePeriod( Dev, VL53L0X_VCSEL_PERIOD_PRE_RANGE, 18);
+//			VL53L0X_SetVcselPulsePeriod( Dev, VL53L0X_VCSEL_PERIOD_FINAL_RANGE, 14);
+//	    }
 //
 //  /* USER CODE END 2 */
 //
@@ -212,43 +222,56 @@
 //  while (1)
 //  {
 //
-//	  if(TIM7_flag == 1)
-//	  {
-//		  TIM7_flag = 0;
-//		  uint64_t curTime = HAL_GetTick(); // 현재시간 저장 44일까지 가능
-//		  uint16_t DiffTime2;
-//
-//
 //		   for (int i = 0; i < NUM_SENSOR; i++) {
-//		       Dev[i] = &vl53l0x_s[i];
-//		       HAL_I2C_Master_Transmit(&hi2c1, TCA9548A_ADDR, &tca_ch[i], 1, 1000); // Switch to channel i
-//		 		  uint64_t curTime3 = HAL_GetTick(); // 현재시간 저장 44일까지 가능
 //
-//		       VL53L0X_PerformSingleRangingMeasurement(Dev[i], &RangingData);
-////		       if (RangingData.RangeStatus == 0) {
+//				uint8_t q = i / 8;
+//				uint8_t r = i % 8;
+//
+//				if(q == 0){
+//					HAL_I2C_Master_Transmit(&hi2c1, 0x71 << 1, &tca_ch_reset, 1, 1000);
+//					HAL_I2C_Master_Transmit(&hi2c1, 0x72 << 1, &tca_ch_reset, 1, 1000);
+//					HAL_I2C_Master_Transmit(&hi2c1, 0x70 << 1, &tca_ch[r], 1, 1000);
+//
+//				}else if(q == 1){
+//					HAL_I2C_Master_Transmit(&hi2c1, 0x70 << 1, &tca_ch_reset, 1, 1000);
+//					HAL_I2C_Master_Transmit(&hi2c1, 0x72 << 1, &tca_ch_reset, 1, 1000);
+//					HAL_I2C_Master_Transmit(&hi2c1, 0x71 << 1, &tca_ch[r], 1, 1000);
+//
+//				}else if(q == 2){
+//					HAL_I2C_Master_Transmit(&hi2c1, 0x70 << 1, &tca_ch_reset, 1, 1000);
+//					HAL_I2C_Master_Transmit(&hi2c1, 0x71 << 1, &tca_ch_reset, 1, 1000);
+//					HAL_I2C_Master_Transmit(&hi2c1, 0x72 << 1, &tca_ch[r], 1, 1000);
+//				}
+//
+//		       Dev = &vl53l0x_s[i];
+//
+//		       VL53L0X_PerformContinuousRangingMeasurement(Dev, &RangingData);
+////		       VL53L0X_PerformSingleRangingMeasurement(Dev, &RangingData);
+//		       if (RangingData.RangeStatus == 0) {
 //
 //		    	   distance[i] = RangingData.RangeMilliMeter;
-//
-//		           MessageLen = sprintf((char*)Message, "S%d:%d ", i, distance[i]);
+//		           MessageLen = sprintf((char*)Message, "%d ",distance[i]);
 //		           HAL_UART_Transmit(&huart1, Message, MessageLen, 1000);
-//		 		  uint64_t curTime4 = HAL_GetTick(); // 현재시간 저장 44일까지 가능
-//				  DiffTime2 = curTime4 - curTime3;
-//
-////		       }
+//		       }else{
+//		           MessageLen = sprintf((char*)Message, "NL ");
+//		           HAL_UART_Transmit(&huart1, Message, MessageLen, 1000);
+//		       }
+//		       if(r == 7){
+//		           MessageLen = sprintf((char*)Message, "/ ");
+//		           HAL_UART_Transmit(&huart1, Message, MessageLen, 1000);
+//		       }
 //		   }
-//		  uint64_t curTime2 = HAL_GetTick(); // 현재시간 저장 44일까지 가능
-//		  uint16_t DiffTime = curTime2 - curTime;
 //
 //
-//		   MessageLen = sprintf((char*)Message, "Time:%d %d \n",DiffTime,DiffTime2);
+//		   MessageLen = sprintf((char*)Message, "\n");
 //		   HAL_UART_Transmit(&huart1, Message, MessageLen, 1000);
 //
-//	  }
+//
 //
 //
 //
 //    /* USER CODE END WHILE */
-//
+//+
 //    /* USER CODE BEGIN 3 */
 //  }
 //  /* USER CODE END 3 */
@@ -304,17 +327,6 @@
 //  {
 //    Error_Handler();
 //  }
-//}
-//
-///**
-//  * @brief NVIC Configuration.
-//  * @retval None
-//  */
-//static void MX_NVIC_Init(void)
-//{
-//  /* TIM7_IRQn interrupt configuration */
-//  HAL_NVIC_SetPriority(TIM7_IRQn, 0, 0);
-//  HAL_NVIC_EnableIRQ(TIM7_IRQn);
 //}
 //
 ///* USER CODE BEGIN 4 */
